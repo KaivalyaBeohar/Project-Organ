8b3c61b500dbacffad23f0a27a4fc578815769d7
yash-7575 <yashbhagyawant70@gmail.com>
2025-01-27 19:29:55 +0530

    dashboard added

--- anatomy/anatomy/urls.py
+++ anatomy/anatomy/urls.py
@@ -17,10 +17,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 import myapp.urls
+import authentication.urls
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',include(myapp.urls))
+    path('',include(myapp.urls)),
+    path('login/',include(authentication.urls))
 
 ]
--- anatomy/myapp/urls.py
+++ anatomy/myapp/urls.py
@@ -3,8 +3,6 @@ from myapp import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('register/', views.RegisterView, name='register'),
-    path('login/', views.LoginView, name='login'),
-    path('logout/', views.LogoutView, name='logout'),
-    path('dashboard/', views.DashboardView, name='dashboard'),
+    path('about/', views.about, name='about'),
+    path('information/', views.information, name='information'),
 ]
